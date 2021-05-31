356e6d987cb8e2026b78b327e6ddb0a9b48ff213
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2021-05-30 21:11:25 -0300

    Improving seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,62 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user1 = User.create!(
+  first_name: "Rodrigo",
+  last_name: "Yamashita",
+  email: "rodrigomarcondes2000@gmail.com",
+  username: "rodrigo"
+)
+
+user2 = User.create!(
+  first_name: "Alberto",
+  last_name: "Pasqualini",
+  email: "pasqualini@pdt.org",
+  username: "pasqualini"
+)
+
+Bond.create(user: user1, friend: user2, state: Bond::FOLLOWING)
+Bond.create(user: user2, friend: user1, state: Bond::FOLLOWING)
+
+place = Place.create!(
+  locale: "en",
+  name: "Tangamandápio",
+  place_type: "hotel",
+  coordinate: "POINT ( 112.73 -7.25 0 )"
+)
+
+post = Post.create!(
+  user: user1,
+  postable: Status.new(
+    text: "Aoooo! Que doidera"
+  )
+)
+
+Post.create!(
+  user: user2,
+  postable: Status.new(
+    text: "Realmente, é muito loko!"
+  ),
+  thread: post
+)
+
+Post.create!(
+  user: user1,
+  postable: Status.new(
+    text: "Realmente"
+  ),
+  thread: post
+)
+
+Post.create!(
+  user: user2,
+  postable: Status.new(
+    text: "Né..."
+  ),
+  thread: post
+)
+
+Post.create!(
+  user: user1,
+  postable: Sight.new(
+    place: place,
+    activity_type: Sight::CHECKIN
+  )
+)
