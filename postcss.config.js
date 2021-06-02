ba261083ad291ab75472889879af724537c6880d
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2021-06-01 21:56:22 -0300

    Trying to improve existing styles (but it's breaking)

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: [
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
-    require('tailswindcss')('./app/javascript/css/tailswind.js'),
+    require('tailwindcss')('./app/javascript/css/tailwind.js'),
     require('autoprefixer'),
     require('postcss-preset-env')({
       autoprefixer: {
