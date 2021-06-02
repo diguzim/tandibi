b54d85ff5309b75fc94b64aee6c79b3cbbaee95c
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2021-06-02 14:41:27 -0300

    Improving forgotten password

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         authentication_keys: [:login],
+         reset_password_keys: [:login]
+  
+  attr_writer :login
+
+  validates_uniqueness_of :email
   validates :email, uniqueness: true, format: {
     with: URI::MailTo::EMAIL_REGEXP,
     message: "must be a valid email address"
@@ -53,6 +59,39 @@ class User < ApplicationRecord
   source: :user
 
   before_save :ensure_proper_name_case
+
+  def login
+    @login || username || email
+  end
+
+  def self.find_authenticatable(login)
+    where("username = :value  OR email = :value", value: login).first
+  end
+
+  def self.find_for_database_authentication(conditions)
+    conditions = conditions.dup
+    login = conditions.delete(:login).downcase
+    find_authenticatable(login)
+  end
+
+  def self.send_reset_password_instructions(conditions)
+    recoverable = find_recoverable_or_init_with_errors(conditions)
+    if recoverable.persisted?
+      recoverable.send_reset_password_instructions
+    end
+    recoverable
+  end
+
+  def self.find_recoverable_or_init_with_errors(conditions)
+    conditions = conditions.dup
+    login = conditions.delete(:login).downcase
+    recoverable = find_authenticatable(login)
+    unless recoverable
+      recoverable = new(login: login)
+      recoverable.errors.add(:login, login.present? ? :not_found : :blank)
+    end
+    recoverable
+  end
 
   private
 
